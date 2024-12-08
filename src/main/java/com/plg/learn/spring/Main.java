c12166352e8679a8fefce631ab4c8dbdf962c734
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-08 17:22:58 +0100

    Created CRUD to postgrep

--- src/main/java/com/plg/learn/spring/Main.java
+++ src/main/java/com/plg/learn/spring/Main.java
@@ -2,19 +2,58 @@ package com.plg.learn.spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Objects;
 
 @SpringBootApplication
 @RestController
+@RequestMapping("api/v1/customers")
 public class Main {
+    private final CustomerRepository customerRepository;
+
+    public Main(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
     public static void main(String[] args){
 SpringApplication.run(Main.class, args);
     }
 
+    @GetMapping
+    public List<Customer> getCostomers(){
+    // return List.of();
+        return customerRepository.findAll();
+    }
+
+    @PostMapping
+    public void addCustomer(@RequestBody NewCustomerRequest request){
+Customer customer = new Customer();
+customer.setName(request.name);
+customer.setEmail(request.email);
+customer.setAge(request.age);
+customerRepository.save(customer);
+    }
+    @DeleteMapping("{customerId}")
+    public void deleteCustomer(@PathVariable("customerId") Integer id){
+        customerRepository.deleteById(id);
+    }
+
+    @PutMapping("{customerId}")
+    public void updateCustomer(@PathVariable("customerId") Integer id, @RequestBody NewCustomerRequest request){
+        Customer  customer = new Customer();
+        customer.setId(id);
+        customer.setName(request.name);
+        customer.setEmail(request.email);
+        customer.setAge(request.age);
+        customerRepository.save(customer);
+    }
+
+    record NewCustomerRequest(
+      String name, String email, Integer age
+    ){
+    }
     @GetMapping("/greet")
     public Greetresponce greet(){
         return new Greetresponce("Hello", List.of("Java", "Golang"),new Person("Peter",52,3055.55));
