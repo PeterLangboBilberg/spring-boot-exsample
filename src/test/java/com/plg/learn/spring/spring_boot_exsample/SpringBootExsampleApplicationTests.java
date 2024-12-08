405b3b1400d0b50954a6fe22821a77403a054ea0
Peter Bilberg <peter.langbo.bilberg.beumer@gmail.com>
2024-12-08 11:17:13 +0100

    Created a new Web project from start.spring.io

---
+++ src/test/java/com/plg/learn/spring/spring_boot_exsample/SpringBootExsampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plg.learn.spring.spring_boot_exsample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootExsampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
